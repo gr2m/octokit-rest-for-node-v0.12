669cd0f5a500e5b9101e3b12c7ffa6037a8568e0
Gregor <gregor@martynus.net>
2018-02-28 11:10:19 -0800

    authenticate in test, install v0.12 on travis before using

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 This is just an experiment, please move on :)
 
 Goal is to make `@octokit/rest` work in Node v0.12, for an internal project, don’t ask ¯\\\_(ツ)\_/¯
+
+Run test in Nove v0.12 with
+
+```
+GH_TOKEN=... node test.js
+```
--- test.js
+++ test.js
@@ -1,5 +1,14 @@
 var octokit = require('./build')()
 
+if (!process.env.GH_TOKEN) {
+  throw new Error('must set GH_TOKEN')
+}
+
+octokit.authenticate({
+  type: 'token',
+  token: process.env.GH_TOKEN
+})
+
 octokit.repos.get({
   owner: 'octokit',
   repo: 'rest.js'
