a7ad333389802a077da7379761f0d46ebc42a502
Gregor <gregor@martynus.net>
2018-02-28 17:16:24 -0800

    feat: initial version

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+// polyfill Buffer.from
+if (!Buffer.from) {
+  Buffer.from = function (data, encoding, length) {
+    return new Buffer(data, encoding, length)
+  }
+}
+
 module.exports = require('@octokit/rest')
